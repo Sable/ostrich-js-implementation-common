a6d091cdc1ac05e4f92b554d633ef754fc44a6f7
Erick <github@ericklavoie.com>
2016-09-22 18:21:10 +0200

    Fixed bugs

--- fletcher_sum.js
+++ fletcher_sum.js
@@ -1,19 +1,35 @@
+function matlab_modulo(x, y) {
+    var n = Math.floor(x/y);
+    return x - n*y;
+}
+
 function fletcher_sum_ndarray(A) {
     var sum1 = 0;
     var sum2 = 0;
 
     if (A.shape.length === 1) {
         for (var i = 0; i < A.size; ++i) {
-            sum1 = (sum1 + A.get(i)) % 255;
-            sum2 = (sum2 + sum1) % 255;
+            sum1 = matlab_modulo((sum1 + A.get(i)),255);
+            sum2 = matlab_modulo((sum2 + sum1),255);
         } 
     } else if (A.shape.length === 2) {
         for (var i = 0; i < A.shape[1]; ++i) {
             for (var j = 0; j < A.shape[0]; ++j) {
-                sum1 = (sum1 + A.get(j,i)) % 255;
-                sum2 = (sum2 + sum1) % 255;
+                sum1 = matlab_modulo((sum1 + A.get(j,i)),255);
+                sum2 = matlab_modulo((sum2 + sum1),255);
             }
         }
+    } else if (A.shape.length === 3) {
+        for (var i = 0; i < A.shape[2]; ++i) {
+            for (var j = 0; j < A.shape[1]; ++j) {
+                for (var k = 0; k < A.shape[0]; ++k) {
+                    sum1 = matlab_modulo((sum1 + A.get(k,j,i)),255);
+                    sum2 = matlab_modulo((sum2 + sum1),255);
+                }
+            }
+        }
+    } else {
+        throw new Error('Unsupported shape of dimension: ' + A.shape.length);
     }
 
     return sum2 * 256 + sum1;
